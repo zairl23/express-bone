1a3599edcb711c5dc5a9f22e8667b56f58b646ec
ney <756127792@qq.com>
2016-04-04 22:41:52 +0800

    add needed packages for basic web develop

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 var express = require('express');
+var http = require('http');
+var path = require('path');
 var app = express();
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+// db
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/bone');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected into mongodb!');
+});
+
+var KittyModel = require('./services/KittyModel.js');
+
+app.set("port", process.env.PORT || 3000);
+// view engine setup
+app.set('views', path.join(__dirname, 'public/views'));
+app.set('view engine', 'jade');
+app.disable("x-powered-by");
+app.locals.appName = "Express Bone";
+app.use(logger("dev"));
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-  res.send('hello world');
+  var kitty = new KittyModel({ name: 'Silence' });
+  console.log(kitty.name); // 'Silence'
+  res.send('hello world,' + kitty.speak());
 });
 
-app.listen(3000, function () {
-  console.log('Express Bone app listening on port 3000!');
-});
+// 启动应用
+http.createServer(app).listen(app.get("port"));
--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
 ### Start Here
 http://expressjs.com/en/starter/hello-world.html
+
+
+开发过程中，每次修改代码都需要停止再重启应用，太过麻烦，所以我们需要安装nodemon。
+  sudo npm install -g nodemon
